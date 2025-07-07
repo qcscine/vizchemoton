3d607331075c19ffde0a4f53b62bbc89116708f1
petruse <petruse@ethz.ch>
2025-07-07 14:33:52 +0200

    updated configuration of setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 from setuptools import setup, find_packages
 
+# setup.py
+import re
+
+with open("vizchemoton/__init__.py") as f:
+    version = re.search(r'__version__ = "(.*?)"', f.read()).group(1)
+
 setup(
     name='vizchemoton',
-    version='0.1.0',
+    version=version,
     packages=find_packages(),
     author='Enric Petrus, Diego Garay-Ruiz',
     author_email= ['enric.petrus@eawag.ch'],
     description='A package for visualizing Chemoton data.',
     long_description=open('README.md').read(),
-    url='https://github.com/petrusen/vizchemoton',
+    url='https://github.com/qcscine/vizchemoton',
     license="BSD (3-clause)",
     include_package_data=True,
     keywords='chemistry visualization chemoton',
-    project_urls={
-        'Source': 'https://github.com/petrusen/vizchemoton',
-        'Tracker': 'https://github.com/petrusen/vizchemoton/issues',
-    },
 )
 
