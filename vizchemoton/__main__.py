06810b5d7fc771e3938107fbe05f2d3b7dcf16b3
Enric Petrus <enricpz@icloud.com>
2026-07-16 11:06:34 +0200

    first working version - compound cost coloring

--- vizchemoton/__main__.py
+++ vizchemoton/__main__.py
@@ -88,7 +88,7 @@ def main():
         palette = ["#d01414", "#f0ce0e", "#12ba14"]
         qual_map = dict(zip([0, 1, 2], palette))
     else:
-        palette = "RdYlGn5" #"Viridis256"
+        palette = "RdYlGn4" #"Viridis256"
         #palette = ["#1a9850", "#e6f598", "#fee08b", "#d73027"]
         qual_map = {}
 
