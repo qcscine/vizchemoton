46cbd6170edc5db596a45e49676d16b52333a57c
Enric Petrus <enricpz@icloud.com>
2026-08-07 13:16:17 +0200

    clean up

--- vizchemoton/__main__.py
+++ vizchemoton/__main__.py
@@ -161,22 +161,7 @@ def main():
             recursive_cost=pf_costs_recu,
             verbose=verbose,
         )
-<<<<<<< HEAD
-    #    reactions, compounds = get_reactions_and_compounds(
-    #        manager,
-    #        pathfinder,
-    #        model,
-    #        (rdkitobj, smiles_method),
-    #        rdkitprop,
-    #        databases=databases,
-    #        verbose=verbose,
-    #    )
-
-    # create reactions and compounds
-    if reactions_mode == "write" and compounds_mode == "write" and db_active:
-=======
     if reactions_mode == "write" and compounds_mode == "write":
->>>>>>> 35d8407dbda28332eaec5dc9229ebcc6ebf301a2
         reactions, compounds = get_reactions_and_compounds(
             manager,
             pathfinder,
@@ -187,7 +172,6 @@ def main():
             verbose=verbose,
         )
         write_compound_reactions_files(
-<<<<<<< HEAD
             reactions,
             compounds,
             reactions_file,
@@ -196,32 +180,12 @@ def main():
         )
     elif compounds_mode == "read" and reactions_mode == "read":
         reactions, compounds = read_compound_reactions_files(
-            reactions_file, compounds_file, verbose=verbose
-        )
-=======
-               reactions,
-               compounds,
-               reactions_file,
-               compounds_file,
-               verbose=verbose,
-           )
->>>>>>> 35d8407dbda28332eaec5dc9229ebcc6ebf301a2
+            reactions_file, compounds_file, verbose=verbose)
     elif compounds_mode == "review":
-        reactions, compounds = read_compound_reactions_files(
-            reactions_file, compounds_file, verbose=verbose
-        )
         compounds = review_compound_file(compounds_file)
-<<<<<<< HEAD
-=======
-    elif compounds_mode == "read" and reactions_mode == "read":
-        reactions, compounds = read_compound_reactions_files(
-            reactions_file, compounds_file, verbose=verbose
-        )
->>>>>>> 35d8407dbda28332eaec5dc9229ebcc6ebf301a2
     elif compounds_mode == "upgrade":
         reactions, compounds = read_compound_reactions_files(
-            reactions_file, compounds_file, verbose=verbose
-        )
+            reactions_file, compounds_file, verbose=verbose)
         compounds = upgrade_compound_file(compounds_file, rdkitprop, databases)
     
     # Preparing HTML GUI
